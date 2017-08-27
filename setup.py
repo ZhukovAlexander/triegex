bef1a4d95712f0c4605fa31585ea8613667d3a3d
Alexander Zhukov <zhukovaa90@gmail.com>
2017-08-27 22:41:56 +0300

    Add setup.py script

---
+++ setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup
+
+setup(
+    name="triegex",
+    keywords="python regular expressions trie",
+    use_scm_version=True,
+    setup_requires=['setuptools_scm'],
+    py_modules=['triegex'],
+)
